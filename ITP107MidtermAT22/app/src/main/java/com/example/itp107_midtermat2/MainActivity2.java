63799a23c2464ec0458af1e1e7547622029e084a
JamesRivadinera <91047285+JamesRiva@users.noreply.github.com>
2024-04-30 09:36:34 +0800

    Add files via upload

---
+++ ITP107MidtermAT22/app/src/main/java/com/example/itp107_midtermat2/MainActivity2.java
@@ -0,0 +1,38 @@
+package com.example.itp107_midtermat2;
+
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main2);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Bundle bundle = getIntent().getExtras();
+        String name = "This is your Full name";
+        if(bundle != null){
+            name = bundle.getBundle("fullname").toString();
+            setFullName(name);
+        }
+    }
+
+    private void setFullName(String fullName){
+        TextView textView = (TextView) findViewById(R.id.textView2);
+        textView.setText(fullName);
+    }
+}
